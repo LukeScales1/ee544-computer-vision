8e64a037297a42f352465256cd0230d602962194
luke <luke@nory.ai>
2021-03-27 13:16:27 -0700

    added dimension parameter to image loading for future experimentation, model build of VGG-lite

--- vgglite_1/load_data.py
+++ vgglite_1/load_data.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.keras.utils.conv_utils import normalize_tuple
 try:
     from utils import data_fldr
 except ModuleNotFoundError:
@@ -6,19 +7,23 @@ except ModuleNotFoundError:
     from .utils import data_fldr
 
 
-def build_dataset(fldr):
+def build_dataset(fldr, dims=None):
+    if dims is None:
+        dims = (244, 244)  # default to VGG-16 input dimensions
+    else:
+        dims = normalize_tuple(value=dims, n=2, name="input_dims")
     return tf.keras.preprocessing.image_dataset_from_directory(
         directory=f"{data_fldr}/{fldr}",
         labels="inferred",
         label_mode="categorical",
         color_mode="grayscale",
-        image_size=(244, 244),
+        image_size=dims,
     )
 
 
-def load():
+def load(img_dims):
     return (
-        build_dataset("train"),
-        build_dataset("validation"),
-        build_dataset("test"),
+        build_dataset("train", img_dims),
+        build_dataset("validation", img_dims),
+        build_dataset("test", img_dims),
     )
