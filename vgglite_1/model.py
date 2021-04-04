b378ae32fbb00c53da3ab05a3795c0379f43d37c
LukeScales1 <luke.scales2@gmail.com>
2021-04-03 18:25:31 -0700

    tweaking image aug, using batch norm dropout and varying activation funcs

--- vgglite_1/model.py
+++ vgglite_1/model.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import torch
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Input, Dropout
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, BatchNormalization, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.python.keras.utils.conv_utils import normalize_tuple
 from sklearn.metrics import classification_report, confusion_matrix
@@ -60,7 +60,8 @@ def baseline_model(input_dims, n_classes, include_rescale=False):
     return model
 
 
-def build_model(input_dims, n_classes, include_rescale=False, activation_func=None, output_activation=None, dropout=None):
+def build_model(input_dims, n_classes, include_rescale=False, activation_func=None, output_activation=None, 
+                dropout=True, batch_norm=True):
     if input_dims is None:
         input_dims = (244, 244)  # default to VGG-16 input dimensions
     else:
@@ -71,31 +72,43 @@ def build_model(input_dims, n_classes, include_rescale=False, activation_func=No
     if output_activation is None:
         output_activation = "relu"
 
-    model = Sequential(name="VGG-liter")
+    model = Sequential(name="VGG-lite")
     if include_rescale:
         # to support using tf.keras.preprocessing.image_dataset_from_directory which doesn't have rescale built in
         model.add(
             keras.layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(input_dims[0], input_dims[1], 1))
         )
-        model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu',
+        model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation=activation_func,
                          kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
     else:
-        model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu',
+        model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation=activation_func,
                          input_shape=(input_dims[0], input_dims[1], 1),
                          kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
-
-    model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu',
+    if batch_norm:
+        model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation=activation_func,
                      kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
+    if batch_norm:
+        model.add(BatchNormalization(axis=-1))
     model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu',
+    if dropout:
+        model.add(Dropout(0.2, seed=SEED))
+    model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation=activation_func,
                      kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
-    model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu',
+    if batch_norm:
+        model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation=activation_func,
                      kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
+    if batch_norm:
+        model.add(BatchNormalization(axis=-1))
     model.add(MaxPool2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(units=512, activation='relu', kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
-    # model.add(Dropout(0.4, seed=SEED))
-    model.add(Dense(n_classes, activation='relu', kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
+    model.add(Dense(units=512, activation=activation_func, kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
+    if batch_norm:
+        model.add(BatchNormalization(axis=-1))
+    if dropout:
+        model.add(Dropout(0.5, seed=SEED))
+    model.add(Dense(n_classes, activation=output_activation, kernel_initializer=tf.keras.initializers.GlorotUniform(seed=SEED)))
     return model
 
 
@@ -188,8 +201,25 @@ if __name__ == "__main__":
     import json
     import load_data
 
+    # EXPERIMENT CONFIG #
     save = True
-    experiment_name = "baseline_244"
+    activation = 'elu'
+    output_activation = 'softmax'
+    use_dropout = True
+    use_batch_norm = True
+    experiment_name = "image_aug_lrschedule_elu_softmax_dropout_batchnorm"
+
+    epochs = 500
+    # learning_rate = 0.001
+    # decay = learning_rate/epochs
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=0.01,
+        decay_steps=1500,
+        decay_rate=0.96,
+        staircase=True)
+    opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+    # opt = tf.keras.optimizers.Adam()
+
     exp_stamp = f"{experiment_name}-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
     checkpoint_filepath = f'model_checkpoints/{exp_stamp}'
 
@@ -201,9 +231,9 @@ if __name__ == "__main__":
         save_best_only=True)
     # logdir = f"logs/{exp_stamp}"
     # tboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
-    # early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+    early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
 
-    callbacks = [model_checkpoint]
+    callbacks = [model_checkpoint, early_stop_callback]
 
 
 
@@ -232,35 +262,28 @@ if __name__ == "__main__":
     #         # Memory growth must be set before GPUs have been initialized
     #         print(e)
 
-    # data_gen_options = {
-    #     "rotation_range": 20,
-    #     "width_shift_range": 0.2,
-    #     "height_shift_range": 0.2,
-    #     "horizontal_flip": True
-    # }
     data_gen_options = {
-        "rescale": 1. / 255
-        # use this alone to *not* augment the dataset and train/test models quicker - for prototyping
+        "rotation_range": 30,
+        "width_shift_range": 0.2,
+        "height_shift_range": 0.2,
+        "shear_range": 0.2,
+        "zoom_range": 0.1,
+        "horizontal_flip": True
     }
+    # data_gen_options = {
+    #     "rescale": 1. / 255
+    #     # use this alone to *not* augment the dataset and train/test models quicker - for prototyping
+    # }
 
     reset_random()
 
-    input_dims = 244
-    (train_data, val_data, test_data) = load_data.load(input_dims)
-    model = build_model(input_dims, n_classes=len(train_data.class_names), include_rescale=True)
-    # (train_data, val_data, test_data) = load_data.load_data_gen(input_dims, seed=SEED, shuffle=True, **data_gen_options)
-    # model = build_model(input_dims=input_dims, n_classes=len(train_data.class_indices.keys()))
+    input_dims = 32
+    # (train_data, val_data, test_data) = load_data.load(input_dims)
+    # model = build_model(input_dims, n_classes=len(train_data.class_names), include_rescale=True)
+    (train_data, val_data, test_data) = load_data.load_data_gen(input_dims, seed=SEED, shuffle=True, **data_gen_options)
+    model = build_model(input_dims=input_dims, n_classes=len(train_data.class_indices.keys()),
+                        activation_func=activation, output_activation=output_activation)
 
-    epochs = 10
-    # learning_rate = 0.001
-    # decay = learning_rate/epochs
-    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    #     initial_learning_rate=0.01,
-    #     decay_steps=1500,
-    #     decay_rate=0.96,
-    #     staircase=True)
-    # opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
-    opt = tf.keras.optimizers.Adam()
     model.compile(optimizer=opt,
                   loss=tf.keras.losses.CategoricalCrossentropy(),
                   metrics=['accuracy'])
